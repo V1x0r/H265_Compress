7fa3959b2c916d342f8da0ebc70906c477ba07a4
V1x0r <V1x0r87@gmail.com>
2018-01-13 12:11:28 -0700

    in progress update
    disabled renaming of files until i'm ready to begin using ffmpeg.  this should allow me to test without hiccups and constantly having to rename files.

--- !Rebuild.ps1
+++ !Rebuild.ps1
@@ -1,4 +1,4 @@
-﻿clear-history
+clear-history
 $StrHomeDir = ($(get-location).Path + "\");
 $StrResourceDir = ($StrHomeDir + "Resources");
 $MainScript = ($StrHomeDir + "!Rebuild.ps1");
@@ -11,9 +11,11 @@ $FFMPEG = $StrResourceDir + "\FFmpeg.exe"
 $ExcludeList = ("*.exe", "*.dll", "*.jpg", "*.png", "*.xml", "*.nfo")
 Clear-Host
 
+#ToDo - Check Directory Structure before running?
 
-# $newvid = [io.path]::ChangeExtension($oldvid.FullName, '.mp4')
-
+#========================================================================================
+#=====================  Traverse Folders Finding Files To Rebuild =======================
+#========================================================================================
 
 function TraverseFolders
 {
@@ -23,74 +25,113 @@ function TraverseFolders
     #>
     Param($WorkingDir)
 
-    $MovieFolder = Get-ChildItem $WorkingDir -Exclude $ExcludeList |`
-        Where-Object {$_.Length -ge 5MB}
-    $SeasonsFolders = Get-ChildItem $WorkingDir -recurse -directory -depth 1 |`
-        Where-Object { $_.name -match "Season*"}
-#================================================================================
+#=================================== Movies =====================================
+
+ $MovieFolder = Get-ChildItem $WorkingDir -Exclude $ExcludeList |`
+        Where-Object {$_.Length -ge 5MB} | Where-Object {!($_.Name.Contains("old_"))}
     ForEach ($Film in $MovieFolder)
     {
-    if ($Film.Name -Notlike "old_*") 
-        {
-             if (!(("old_" + $Film.Name) | Test-Path))
-                {
-                     CheckMedia -MediaInput $Film -Type "Movie"
-                     #Get-Item -Path $Film.FullName -Filter 
-                }
-        }
+        if (!(("old_" + $Film.Name) | Test-Path))
+            {
+                RenameMKV -MediaInput $Film -Type "Movie"
+                #Get-Item -Path $Film.FullName -Filter 
+            }
     }
-#================================================================================
+
+#================================== TV Shows ====================================
+
+ $SeasonsFolders = Get-ChildItem $WorkingDir -recurse -directory -depth 1 |`
+        Where-Object { $_.name -match "Season*"}
   ForEach ($Season in $SeasonsFolders)
     {
-        $Episodes = Get-ChildItem $Season -File  | Where-Object {$_.Length -ge 5MB} |`
-        Where-Object {!($_.Name.Contains("old_"))} #| Where-Object {("old_" + $_.Name) -like $_.Name}
+        $Episodes = Get-ChildItem $Season -Exclude $ExcludeList | Where-Object {$_.Length -ge 5MB} |`
+        Where-Object {!($_.Name.Contains("old_"))}
         ForEach ($Episode in $Episodes)
         {
-   If (!(Test-Path ($Season.fullname + "\" + "old_" + $Episode.name)))
-   {
-   # write-output $Episodes.count  
-   CheckMedia -MediaInput $Episode -Type "TV"
-   }
-   }
+           If (!(Test-Path ($Season.fullname + "\" + "old_" + $Episode.name)))
+               {
+               # write-output $Episodes.count  
+               RenameMKV -MediaInput $Episode -Type "TV"
+               }
+           }
         }
 }
 
-
-#=========================================================================================================================
-function CheckMedia
+#========================================================================================
+#============================== Rename MKV For Backup ===================================
+#========================================================================================
+function RenameMKV
 {
     <#
     .SYNOPSIS
-    Filters Videos for conversion
+    Determines if Media Needs to be renamed
     #>
     
    Param($MediaInput,$Type)
-    
-    write-output $MediaInput.name
-    Write-Output $Type
-    #if ($MediaInput.Name -notlike "old_*") 
-  #  { 
-        #rename if already mkv exists
-       ## if ($sf.name -ilike "*.mkv")
-         #   {
-                   # Rename-Item -NewName ($sf.name –replace $sf.name,("old_" + $sf.name)) -path $SubFolders
-         #   } 
-   # }
+   if ($MediaInput.Extension -eq ".mkv")
+   {
+        $OriginalInput = ("old_" + $MediaInput.name)
+      <# $MediaInput = Rename-Item $MediaInput.FullName $OriginalInput
+        #Write-Output $OriginalInput
+        if (Test-Path $OriginalInput)
+        {        
+        $MediaInput = Get-Item $OriginalInput
+        } Else {
+        $MediaInput = Get-Item ($Season.FullName + "\" + $OriginalInput)
+        }
+        #>
+        Get-Poster -MediaInput $MediaInput -Type $Type
+    }
 }
-#=========================================================================================================================
 
-
-#=========================================================================================================================
-function Get-Poster ()
+#========================================================================================
+#================================= Determine Poster =====================================
+#========================================================================================
+function Get-Poster
 {
+    <#.SYNOPSIS
+    Looks for locally storated Posters exported from Kodi.
+    MovieName-poster.jpg & SeasonXX.jpg in working dir.
+    #>
+
+    Param($MediaInput,$Type)
+    $MoviePoster = ($MediaInput.BaseName + "-poster.jpg")
+    $SeasonPoster = (($Season.BaseName + "-poster.jpg").ToLower().replace(" ",""))
+    If ($Type -eq "Movie")
+        {
+        if (Test-Path $MoviePoster)
+            {
+            Write-Output $MoviePoster
+            }Else{
+            Write-OutPut ("ERROR " + "'" + $MoviePoster + "'" + " Not Present")
+            }
+        }
+    If ($Type -eq "TV")
+        {
+        if (Test-Path $SeasonPoster)
+            {
+            Write-Output $SeasonPoster
+            }Else{
+            Write-OutPut ("ERROR " + "'" + $SeasonPoster + "'" + " Not Present")
+            }
+        }
 }
-#=========================================================================================================================
 
-
-
-
-#=========================================================================================================================
-function CleanupWorkingDir ()
+#========================================================================================
+#================================= Parse Kodi .NFO ======================================
+#========================================================================================
+function ParseKodiNFO
+{
+    <#
+    .SYNOPSIS
+    This part of the Script will use Kodi's .NFO output to embed various information
+    Into the Media File.
+    #>
+}
+#========================================================================================
+#============================= Cleanup Working Directory ================================
+#========================================================================================
+function CleanupWorkingDir
 {
     <#
     .SYNOPSIS
