c593912ea761041b3b3470fcf0c3e912d405f2b0
V1x0r <V1x0r87@gmail.com>
2018-01-16 17:02:52 -0700

    Update !Rebuild.ps1
    pretty sure I finished the xml properties.

--- !Rebuild.ps1
+++ !Rebuild.ps1
@@ -48,12 +48,12 @@ function TraverseFolders
         Where-Object {!($_.Name.Contains("old_"))}
         ForEach ($Episode in $Episodes)
         {
-           If (!(Test-Path ($Season.fullname + "\" + "old_" + $Episode.name)))
+            If (!(Test-Path ($Season.fullname + "\" + "old_" + $Episode.name)))
                {
                # write-output $Episodes.count  
                RenameMKV -MediaInput $Episode -Type "TV"
                }
-           }
+            }
         }
 }
 
@@ -101,7 +101,7 @@ function Get-Poster
         {
         if (Test-Path $MoviePoster)
             {
-            Write-Output $MoviePoster
+            ParseKodiNFO -MediaInput $MediaInput -Type $Type
             }Else{
             Write-OutPut ("ERROR " + "'" + $MoviePoster + "'" + " Not Present")
             }
@@ -110,7 +110,7 @@ function Get-Poster
         {
         if (Test-Path $SeasonPoster)
             {
-            Write-Output $SeasonPoster
+            ParseKodiNFO -MediaInput $MediaInput -Type $Type
             }Else{
             Write-OutPut ("ERROR " + "'" + $SeasonPoster + "'" + " Not Present")
             }
@@ -127,7 +127,135 @@ function ParseKodiNFO
     This part of the Script will use Kodi's .NFO output to embed various information
     Into the Media File.
     #>
+
+    Param($MediaInput,$Type)
+    $Comment = "Brought To you By V1x0r"
+    [string[]]$array = @("TITLE","SHOWTITLE","SEASON","EPISODE","PLOT","TAGLINE","MPAA","PREMIERED","STUDIO","GENRE")
+    $MovieNFO = ($MediaInput.BaseName + ".nfo")
+    $EpisodeNFO = ($Season.fullname + "\" + $Episode.BaseName + ".nfo")
+    If ($Type -eq "Movie")
+        {
+        $VideoxmlOutput = New-Object System.XML.XmlTextWRiter(($StrHomeDir + $MediaInput.BaseName + ".xml"),$Null)
+        $VideoxmlOutput.Formatting = 'Indented'
+        $VideoxmlOutput.Indentation = 1
+        $VideoxmlOutput.IndentChar = "`t"
+        $VideoxmlOutput.WriteStartDocument()
+        $VideoxmlOutput.WriteStartElement('Tags')
+        if (Test-Path $MovieNFO)
+            {
+
+            [xml]$MovieNFOXML = Get-Content $MovieNFO
+            $MovieNFOArr = Select-XML -XML $MovieNFOXML -XPath "//movie"| Select-Object -ExpandProperty Node
+            #$MovieNFOArr.actor.name # | Select-Object "title"
+            foreach ($item in $array) 
+                {
+                if ($MovieNFOArr.$item -ne $null)
+                    {
+                    $MovieNFOContent = $MovieNFOArr.$item.replace("&","and").replace("Rated ", "").trim()
+                    $item = $item.replace("PLOT","SYNOPSIS").replace("MPAA","LAW_RATING").replace("TAGLINE","SUMMARY").replace("PREMIERED","DATE_RELEASED").replace("STUDIO","PRODUCTION_STUDIO")
+                    $VideoxmlOutput.WriteStartElement('Tag')
+                    $VideoxmlOutput.writestartelement('Simple')
+                    $VideoxmlOutput.WriteElementString('Name',$item)
+                    $VideoxmlOutput.WriteElementString('String',$MovieNFOContent)
+                    $VideoxmlOutput.WriteEndElement()
+                    $VideoxmlOutput.WriteEndElement()
+                    }
+                 }
+            $VideoxmlOutput.WriteStartElement('Tag')
+            $VideoxmlOutput.writestartelement('Simple')
+            $VideoxmlOutput.WriteElementString('Name',"COMMENT")
+            $VideoxmlOutput.WriteElementString('String',$Comment)
+            $VideoxmlOutput.WriteEndElement()
+            $VideoxmlOutput.WriteEndElement()
+            $VideoxmlOutput.WriteEndDocument()
+            $VideoxmlOutput.Flush()
+            $VideoxmlOutput.Close()
+            Compress_Media -MediaInput $MediaInput -Type $Type
+            }Else{
+            $VideoxmlOutput.WriteStartElement('Tag')
+            $VideoxmlOutput.writestartelement('Simple')
+            $VideoxmlOutput.WriteElementString('Name',"TITLE")
+            $VideoxmlOutput.WriteElementString('String',$MediaInput.BaseName)
+            $VideoxmlOutput.WriteEndElement()
+            $VideoxmlOutput.WriteEndElement()
+            $VideoxmlOutput.WriteStartElement('Tag')
+            $VideoxmlOutput.writestartelement('Simple')
+            $VideoxmlOutput.WriteElementString('Name',"COMMENT")
+            $VideoxmlOutput.WriteElementString('String',$Comment)
+            $VideoxmlOutput.WriteEndDocument()
+            $VideoxmlOutput.Flush()
+            $VideoxmlOutput.Close()
+            Write-OutPut ("ERROR " + "'" + $MovieNFO + "'" + " Not Present - Created Basic xml")
+            Compress_Media -MediaInput $MediaInput -Type $Type
+            }
+        }
+    If ($Type -eq "TV")
+        {
+        $EpisodexmlOutput = New-Object System.XML.XmlTextWRiter(($Season.fullname + "\" + $Episode.BaseName + ".xml"),$Null)
+        $Episodexml = ($Season.fullname + "\" + $Episode.BaseName + ".xml")
+        $EpisodexmlOutput.Formatting = 'Indented'
+        $EpisodexmlOutput.Indentation = 1
+        $EpisodexmlOutput.IndentChar = "`t"
+        $EpisodexmlOutput.WriteStartDocument()
+        $EpisodexmlOutput.WriteStartElement('Tags')
+        if (Test-Path $EpisodeNFO)
+            {
+            [xml]$EpisodeNFOXML = Get-Content $EpisodeNFO
+            $EpisodeNFOArr = Select-XML -XML $EpisodeNFOXML -XPath "//episodedetails"| Select-Object -ExpandProperty Node
+            foreach ($item in $array) 
+                {
+                if ($EpisodeNFOArr.$item -ne "")
+                    {
+                    $EpisodeNFOContent = $EpisodeNFOArr.$item.replace("&","and").replace("Rated ", "").trim()
+                    $item = $item.replace("PLOT","SYNOPSIS").replace("MPAA","LAW_RATING").replace("TAGLINE","SUMMARY").replace("PREMIERED","DATE_RELEASED").replace("STUDIO","PRODUCTION_STUDIO")
+                    $EpisodexmlOutput.WriteStartElement('Tag')
+                    $EpisodexmlOutput.writestartelement('Simple')
+                    $EpisodexmlOutput.WriteElementString('Name',$item)
+                    $EpisodexmlOutput.WriteElementString('String',$EpisodeNFOContent)
+                    $EpisodexmlOutput.WriteEndElement()
+                    $EpisodexmlOutput.WriteEndElement()
+                    }
+                 }
+            $EpisodexmlOutput.WriteStartElement('Tag')
+            $EpisodexmlOutput.writestartelement('Simple')
+            $EpisodexmlOutput.WriteElementString('Name',"COMMENT")
+            $EpisodexmlOutput.WriteElementString('String',$Comment)
+            $EpisodexmlOutput.WriteEndDocument()
+            $EpisodexmlOutput.Flush()
+            $EpisodexmlOutput.Close()
+            Compress_Media -MediaInput $MediaInput -Type $Type
+            }Else{
+            $EpisodexmlOutput.WriteStartElement('Tag')
+            $EpisodexmlOutput.writestartelement('Simple')
+            $EpisodexmlOutput.WriteElementString('Name',"TITLE")
+            $EpisodexmlOutput.WriteElementString('String',$Episode.BaseName)
+            $EpisodexmlOutput.WriteEndElement()
+            $EpisodexmlOutput.WriteEndElement()
+            $EpisodexmlOutput.WriteStartElement('Tag')
+            $EpisodexmlOutput.writestartelement('Simple')
+            $EpisodexmlOutput.WriteElementString('Name',"COMMENT")
+            $EpisodexmlOutput.WriteElementString('String',$Comment)
+            $EpisodexmlOutput.WriteEndDocument()
+            $EpisodexmlOutput.Flush()
+            $EpisodexmlOutput.Close()
+            Write-OutPut ("ERROR " + "'" + $EpisodeNFO + "'" + " Not Present - Created Basic xml")
+            Compress_Media -MediaInput $MediaInput -Type $Type
+            }
+        }
 }
+
+#========================================================================================
+#============================ Begin Media Conversion & tag ==============================
+#========================================================================================
+function Compress_Media
+{
+    <#
+    .SYNOPSIS
+    Compresses the provided media files into H265 inside of matroska
+    #>
+    Param($MediaInput,$Type)
+}
+
 #========================================================================================
 #============================= Cleanup Working Directory ================================
 #========================================================================================
@@ -137,6 +265,7 @@ function CleanupWorkingDir
     .SYNOPSIS
     Cleans the working directory after the script has finished
     #>
+
         Write-Host "Cleaning Up Remaining Resources"
         if (Test-Path $StrResourceDir)
             { 
